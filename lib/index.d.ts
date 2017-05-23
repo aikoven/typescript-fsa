95369c72b94e06b8ee909fe5f546b0aa8e881e7d
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-05-23 11:55:48 +0700

    Build typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,14 @@
 export interface AnyAction {
     type: any;
 }
+export declare type Meta = null | {
+    [key: string]: any;
+};
 export interface Action<P> extends AnyAction {
     type: string;
     payload: P;
     error?: boolean;
-    meta?: Object | null;
+    meta?: Meta;
 }
 export interface Success<P, S> {
     params: P;
@@ -18,10 +21,10 @@ export interface Failure<P, E> {
 export declare function isType<P>(action: AnyAction, actionCreator: ActionCreator<P>): action is Action<P>;
 export interface ActionCreator<P> {
     type: string;
-    (payload: P, meta?: Object | null): Action<P>;
+    (payload: P, meta?: Meta): Action<P>;
 }
 export interface EmptyActionCreator extends ActionCreator<undefined> {
-    (payload?: undefined, meta?: Object | null): Action<undefined>;
+    (payload?: undefined, meta?: Meta): Action<undefined>;
 }
 export interface AsyncActionCreators<P, S, E> {
     type: string;
@@ -29,24 +32,11 @@ export interface AsyncActionCreators<P, S, E> {
     done: ActionCreator<Success<P, S>>;
     failed: ActionCreator<Failure<P, E>>;
 }
-export interface EmptySuccess<S> {
-    result: S;
-}
-export interface EmptyFailure<E> {
-    error: E;
-}
-export interface EmptyAsyncActionCreators<S, E> {
-    type: string;
-    started: EmptyActionCreator;
-    done: ActionCreator<EmptySuccess<S>>;
-    failed: ActionCreator<EmptyFailure<E>>;
-}
 export interface ActionCreatorFactory {
-    (type: string, commonMeta?: Object | null, error?: boolean): EmptyActionCreator;
-    <P>(type: string, commonMeta?: Object | null, isError?: boolean): ActionCreator<P>;
-    <P>(type: string, commonMeta?: Object | null, isError?: (payload: P) => boolean): ActionCreator<P>;
-    async<P, S>(type: string, commonMeta?: Object | null): AsyncActionCreators<P, S, any>;
-    async<undefined, S, E>(type: string, commonMeta?: Object | null): EmptyAsyncActionCreators<S, E>;
-    async<P, S, E>(type: string, commonMeta?: Object | null): AsyncActionCreators<P, S, E>;
+    (type: string, commonMeta?: Meta, error?: boolean): EmptyActionCreator;
+    <P>(type: string, commonMeta?: Meta, isError?: boolean): ActionCreator<P>;
+    <P>(type: string, commonMeta?: Meta, isError?: (payload: P) => boolean): ActionCreator<P>;
+    async<P, S>(type: string, commonMeta?: Meta): AsyncActionCreators<P, S, any>;
+    async<P, S, E>(type: string, commonMeta?: Meta): AsyncActionCreators<P, S, E>;
 }
 export default function actionCreatorFactory(prefix?: string | null, defaultIsError?: (payload: any) => boolean): ActionCreatorFactory;
